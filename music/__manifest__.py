6f430e4a7884bf3991026dd61278a6f7c5b0c5c7
LubaMazurkevich <lubou.mazurkevich@gmail.com>
2021-11-01 16:30:12 +0300

    added album and artist view

--- music/__manifest__.py
+++ music/__manifest__.py
@@ -2,8 +2,12 @@
     'name': 'music',
     'data': ['views/group_view.xml',
              'views/song_view.xml',
+             'views/artist_view.xml',
+             'views/album_view.xml',
              'security/ir.model.access.csv',
              'wizards/import_music_wizard_view.xml'],
     'depends': [],
 }
+
+
 
